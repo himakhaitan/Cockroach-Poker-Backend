3421b77d5d249d7b003f28ba08c08c05947ba336
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-16 03:42:10 +0530

    Game Init

---
+++ game/game.js
@@ -0,0 +1,115 @@
+const SOCKET_EVENTS = require("../utils/socketEvents");
+const { doc, setDoc, getDoc } = require("firebase/firestore");
+const db = require("../firebase/FirebaseService");
+
+const cardTypes = [
+  "BAT",
+  "BEE",
+  "COCKROACH",
+  "TORD",
+  "RAT",
+  "SCORPION",
+  "SPIDER",
+  "BUG",
+];
+
+const cardImages = {
+  BAT: "1.png",
+  BEE: "2.png",
+  COCKROACH: "3.png",
+  TORD: "4.png",
+  RAT: "5.png",
+  SCORPION: "6.png",
+  SPIDER: "7.png",
+  BUG: "8.png",
+};
+
+const deck = [];
+
+for (let i = 0; i < cardTypes.length; i++) {
+  for (let j = 0; j < 8; j++) {
+    deck.push(cardTypes[i]);
+  }
+}
+
+const startGame = async (socket, data) => {
+  // Shuffle Cards
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+
+  // Deal Cards
+  let players = {
+    p1: [],
+    p2: [],
+    p3: [],
+    p4: [],
+  };
+
+  for (let i = 0; i < deck.length; i++) {
+    if (i < 16) {
+      players.p1.push(deck[i]);
+    } else if (i < 32) {
+      players.p2.push(deck[i]);
+    } else if (i < 48) {
+      players.p3.push(deck[i]);
+    } else {
+      players.p4.push(deck[i]);
+    }
+  }
+
+  // Set Turn
+
+  let turn = Math.floor(Math.random() * 4);
+
+  // Set Game State
+
+  const docRef = doc(db, "rooms", data);
+
+  const docSnap = await getDoc(docRef);
+
+  let fetcheddata = docSnap.data();
+
+  fetcheddata.gameState = {
+    turn: turn,
+  };
+
+  fetcheddata.players[0].cards = players.p1;
+  fetcheddata.players[0].lost = [];
+  fetcheddata.players[1].cards = players.p2;
+  fetcheddata.players[1].lost = [];
+  fetcheddata.players[2].cards = players.p3;
+  fetcheddata.players[2].lost = [];
+  fetcheddata.players[3].cards = players.p4;
+  fetcheddata.players[3].lost = [];
+
+  await setDoc(docRef, fetcheddata);
+
+  // EMIT TO ALL PLAYERS IN LOBBY
+  socket.to(data).emit(SOCKET_EVENTS.START_GAME, data);
+
+  fetcheddata.players.forEach((player) => {
+    socket.to(player.userId).emit(SOCKET_EVENTS.LOAD_CARDS, player.cards);
+  });
+};
+
+const displayCards = async (socket, data) => {
+  // EMIT TO ALL PLAYERS IN LOBBY
+
+  const docRef = doc(db, "rooms", data);
+  const docSnap = await getDoc(docRef);
+
+  let fetcheddata = docSnap.data();
+
+  fetcheddata.players.forEach((player) => {
+    socket.to(player.userId).emit(SOCKET_EVENTS.LOAD_CARDS, player.cards);
+  });
+};
+
+module.exports = {
+  startGame,
+  displayCards,
+};
