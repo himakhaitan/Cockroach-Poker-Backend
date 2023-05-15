dd26400f0c7a76ed86d3e268a96348508d1228ee
himakhaitan <himanshukhaitan108@gmail.com>
2023-05-15 23:47:51 +0530

    Create Room Done

--- game/room.js
+++ game/room.js
@@ -2,8 +2,26 @@ const { doc, setDoc } = require("firebase/firestore");
 const db = require("../firebase/FirebaseService");
 const SOCKET_EVENTS = require("../utils/socketEvents");
 
-const createRoom = (socket, data) => {
-  // TODO: CREATE ROOM
+const createRoom = async (socket, data) => {
+  console.log("createRoom", data);
+
+  const docRef = doc(db, "rooms", data.roomId);
+
+  await setDoc(docRef, {
+    roomName: data.roomName,
+    players: [
+      {
+        name: data.userName,
+        avatar: data.userAvatar,
+        isHost: true,
+      },
+    ],
+  });
+  socket.join(data.roomId);
+
+  console.log("Document written with ID: ", docRef.id);
+  socket.emit(SOCKET_EVENTS.ROOM_CREATED, { roomId: docRef.id });
+  console.log("Room Created");
 };
 
 const joinRoom = (socket, data) => {
